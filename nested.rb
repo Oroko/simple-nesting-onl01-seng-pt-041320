b61d61705f6d0520b7a99f202f2ea91ffa772e19
Abel Gechure <aoroko09@gmail.com>
2020-04-24 03:46:57 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -68,11 +68,6 @@ def adding_matz
 
 	programmer_hash = 
  		{
- 		    :yukihiro_matsumoto => {
- 		      :known_for => "Ruby",
- 		      :languages => ["LISP","C"]
- 		      
- 		    },
  		  
         :grace_hopper => {
           :known_for => "COBOL",
@@ -88,7 +83,7 @@ def adding_matz
         }
      }
 
-  programmer_hash[:yukihiro_matsumoto]
+  programmer_hash[:yukihiro_matsumoto] = {
 end
 
 def changing_alan
